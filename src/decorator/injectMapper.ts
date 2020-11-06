740982b5af4eb1121791954f98bec6e14b5f5204
Paulo Henrique <paulohenrique@esenvo.com>
2020-11-06 15:14:50 -0300

    refactor: add the symbol type to context param

--- src/decorator/injectMapper.ts
+++ src/decorator/injectMapper.ts
@@ -5,7 +5,15 @@ import { inject } from "inversify";
  * @param classRef Reference of the class
  * @param contextName Name of the context to scope the class
  */
-export default function injectMapper<T>(classRef: T, contextName?: string) {
-  const prefix = contextName || "";
+export default function injectMapper<T>(
+  classRef: T,
+  contextName?: string | symbol
+) {
+  let prefix = "";
+
+  if (contextName && typeof contextName === "symbol") {
+    prefix = String(contextName).slice(7, -1) || "";
+  }
+
   return inject(Symbol.for(prefix + ((classRef as unknown) as Function).name));
 }
