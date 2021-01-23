3214ba6402890b8245d0a29b787b196e52dfedd6
Lindsay Cerqueira <lindsay@3tecnos.com.br>
2021-01-23 11:41:15 -0300

    refactor: move to decorators directory

---
+++ src/decorators/injectMapper/injectMapper.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { inject } from "inversify";
+
+/**
+ * This method support the use of the class directly
+ * @param classRef Reference of the class
+ * @param contextName Name of the context to scope the class
+ */
+export function injectMapper<T>(classRef: T, contextName?: string | symbol) {
+  let context = "";
+  if (contextName) {
+    if (typeof contextName === "symbol") {
+      context = String(contextName).slice(7, -1) || "";
+    } else {
+      context = contextName;
+    }
+  }
+
+  return inject(
+    Symbol.for(context + ((classRef as unknown) as Function)?.name || "")
+  );
+}
